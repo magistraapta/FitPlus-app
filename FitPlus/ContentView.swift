a67b45326988241744d8e2a0cdef0b44481bbedd
Magistra <magistraagis@gmail.com>
2023-08-15 19:12:08 +0700

    Initial Commit

---
+++ FitPlus/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  FitPlus
+//
+//  Created by magistra aptam on 15/08/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ FitPlus/FitPlusApp.swift
@@ -0,0 +1,17 @@
+//
+//  FitPlusApp.swift
+//  FitPlus
+//
+//  Created by magistra aptam on 15/08/23.
+//
+
+import SwiftUI
+
+@main
+struct FitPlusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
